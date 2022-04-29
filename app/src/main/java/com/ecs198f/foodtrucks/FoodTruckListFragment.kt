5119f8c60f99a753edfcca1ae4e0f1ab0257e691
alexisbl14 <ablydon@ucdavis.edu>
2022-04-28 19:13:53 -0700

    created recycler view on food truck list fragment (home page)

--- app/src/main/java/com/ecs198f/foodtrucks/FoodTruckListFragment.kt
+++ app/src/main/java/com/ecs198f/foodtrucks/FoodTruckListFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.ecs198f.foodtrucks.databinding.ActivityMainBinding
 import java.time.LocalDateTime
 
 class FoodTruckListFragment : Fragment() {
@@ -62,7 +65,13 @@ class FoodTruckListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_food_truck_list, container, false)
+        val view = inflater.inflate(R.layout.fragment_food_truck_list, container, false)
+        view.findViewById<RecyclerView>(R.id.foodTruckListFragmentRecyclerView).apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = FoodTruckListRecyclerViewAdapter(foodTrucks)
+        }
+
+        return view
     }
 
 
--- app/src/main/java/com/ecs198f/foodtrucks/MainActivity.kt
+++ app/src/main/java/com/ecs198f/foodtrucks/MainActivity.kt
@@ -58,14 +58,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
 
         title = "Food Trucks"
-
-        binding.foodTruckListRecyclerView.apply {
-            layoutManager = LinearLayoutManager(context)
-            adapter = FoodTruckListRecyclerViewAdapter(foodTrucks)
-        }
     }
 }
